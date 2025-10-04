e49625ca3ef437220c9a31f73c011fc7a8f055c4
MOHIT-IITP <codingbymohit@gmail.com>
2025-10-04 09:25:07 +0530

    update zsh and add: fuzzy finding

---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle position=right<cr>", desc = "Toggle Neo-tree (right)" },
+    },
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim", -- loads after Neo-tree
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "2.*",
+    config = function()
+      local picker = require("window-picker")
+      picker.setup({
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          bo = {
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            buftype = { "terminal", "quickfix" },
+          },
+        },
+      })
+
+      -- Keymap to pick a window
+      vim.keymap.set("n", "<leader>w", function()
+        local picked_window_id = picker.pick_window() or vim.api.nvim_get_current_win()
+        vim.api.nvim_set_current_win(picked_window_id)
+      end, { desc = "Pick a window" })
+    end,
+  },
+}
