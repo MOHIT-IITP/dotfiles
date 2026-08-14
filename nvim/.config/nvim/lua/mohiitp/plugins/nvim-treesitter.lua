e86bd086ed33fca5552903e2a51a23babac9e0cc
MOHIT-IITP <codingbymohit@gmail.com>
2026-08-14 19:35:21 +0530

    UPDATE: nvim treesitter plugin

--- nvim/.config/nvim/lua/mohiitp/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/mohiitp/plugins/nvim-treesitter.lua
@@ -1,10 +1,18 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        auto_install = true
-    end,
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            auto_install = true
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.opt.foldtext = ""
+            vim.opt.fillchars:append("fold: ")
+            vim.opt.foldlevelstart = 99
+            vim.keymap.set("n", "zo", "zo", { desc = "Open fold" })
+            vim.keymap.set("n", "zf", "zc", { desc = "Close fold" })
+            vim.keymap.set("n", "zR", "zR", { desc = "Open all folds" })
+        end,
+    },
 }
